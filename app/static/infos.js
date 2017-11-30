24784b1e567fe11103a29252cfd2655492437942
was <zxt_zel_npl@163.com>
2017-11-30 16:25:44 +0800

    add 20171130

--- app/static/infos.js
+++ app/static/infos.js
@@ -1,6 +1,7 @@
 import bg20171110 from './img/20171110.jpg'
 import bg20171113 from './img/20171113.jpg'
 import bg20171116 from './img/20171116.jpg'
+import bg20171130 from './img/20171130.jpg'
 
 export default {
     '20171110': {
@@ -20,5 +21,11 @@ export default {
         teacher: '董齐安',
         color: '#F2F2F2',
         top: '10.5rem'
+    },
+    '20171130': {
+        bg: bg20171130,
+        teacher: '马鑫',
+        color: '#FAFAFC',
+        top: '10.8rem'
     }
 }
